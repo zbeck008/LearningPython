2546d3f36197e3ecb782b85b5ba17c4ce0ad83e7
Zack <zbeck008@gmail.com>
2023-08-31 00:04:03 -0600

    made a happy fail option and message

--- Casio Watch Test.py
+++ Casio Watch Test.py
@@ -6,7 +6,11 @@ driver.maximize_window()
 driver.find_element_by_xpath("/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/input").send_keys(
     "5610")
 driver.find_element_by_xpath("/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[3]/div/span/input").click()
-(driver.find_element_by_xpath
- ("/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div[1]/div/span/a/div/img").click())
-driver.save_screenshot("C:\\Users\\Zack\\Desktop\\casiowatchtest.png")
-driver.close()
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div[1]/div/span/a/div/img").click()
+if driver.find_element_by_xpath("/html/body/div[1]/div/div[5]/div[2]/div[1]/div[2]/div[2]/div/div/div[1]/div[1]/div[2]/div[1]/div/h1/span"):
+    print("Casio watch found correctly")
+    driver.close()
+else:
+    print("Casio watch not found!")
+    driver.save_screenshot("C:\\Users\\Zack\\Desktop\\casiowatchtest.png")
+    driver.close()
