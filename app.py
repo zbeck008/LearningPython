a025563a67807febde2d4534921e96657c27f39d
Zack <zbeck008@gmail.com>
2023-08-30 22:33:08 -0600

    amazon test

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from selenium import webdriver
-driver = webdriver.chrome
+
+driver = webdriver.Chrome("C:\\Users\\Zack\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
 driver.get("https://www.amazon.com")
+driver.find_element_by_xpath("/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/input").send_keys("5610")
+driver.find_element_by_xpath("/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[3]/div/span/input").click()
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[4]/div/div/div/div/div[1]/div/span/a/div/img").click()
